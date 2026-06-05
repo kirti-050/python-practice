c4b95c441cd95428843b5d2ca504e58a4cf8d15b
Kirti <kirtisri.vin19@gmail.com>
2026-06-06 00:31:01 +0530

    Merge Sorted Array

---
+++ merge_sorted_array.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        merged = nums1[:m]
+        merged.extend(nums2)
+        merged.sort()
+        nums1[:] = merged
+        print(nums1) 

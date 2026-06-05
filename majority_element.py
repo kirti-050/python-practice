1ab4e25818f0ac893ed5d74598ece4dcd3e60d57
Kirti <kirtisri.vin19@gmail.com>
2026-06-06 01:50:11 +0530

    Majority Element

---
+++ majority_element.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        counts = {}
+
+        for num in nums:
+            if num in counts:
+                counts[num] += 1
+            else:
+                counts[num] = 1
+        
+            if counts[num] > len(nums) //2:
+                return num
